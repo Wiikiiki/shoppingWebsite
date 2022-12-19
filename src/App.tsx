05f02efdfdbff47f5ef59c566bb988e475079b75
Wiikiiki <wiikiiki@outlook.com>
2022-12-19 23:38:59 +0800

    refactor: convert App component from FC to CC

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,67 @@ import cats from "./mockdata/robots.json";
 import Cat from "./components/Cats";
 import ShoppingCart from "./components/ShoppingCart";
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <h1>猫猫购物平台！</h1>
+interface Props {}
+
+interface State {
+  catGallery: any[];
+  count: number;
+}
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      catGallery: [],
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ catGallery: data }));
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>猫猫购物平台！</h1>
+        </div>
+        {/* 理解 setState 的伪异步 */}
+        {/* <button
+          onClick={() => {
+            this.setState(
+              (preState, preProps) => {
+                return { count: preState.count + 1 };
+              },
+              () => {
+                console.log("count:", this.state.count);
+              }
+            );
+            this.setState(
+              (preState, preProps) => {
+                return { count: preState.count + 1 };
+              },
+              () => {
+                console.log("count:", this.state.count);
+              }
+            );
+          }}
+        >
+          click
+        </button>
+        <div>count: {this.state.count}</div> */}
+        <ShoppingCart />
+        <div className={styles.catList}>
+          {this.state.catGallery.map((c) => (
+            <Cat id={c.id} email={c.email} name={c.name} />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.catList}>
-        {cats.map((c) => (
-          <Cat id={c.id} email={c.email} name={c.name} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
